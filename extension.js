337e21a636dcdf3850a2e30a42d7538c6e6bda47
Laurent Perrin <lperrin@tower-research.com>
2019-05-09 10:20:48 +0800

    fix url logging

--- extension.js
+++ extension.js
@@ -459,7 +459,7 @@ function activate( context )
 
         context.subscriptions.push( vscode.commands.registerCommand( 'gerrit-view.openInBrowser', function( item )
         {
-            console.log( item.entry.url );
+            debug( node.source.url );
             vscode.commands.executeCommand( 'vscode.open', vscode.Uri.parse( item.source.url ) );
         } ) );
 
